f9f8db97aaa0da71cb365ddd0cccf57984d325fc
AlanTitor <eldar.kahdgiev2013@yandex.ru>
2026-03-29 17:10:23 +0300

    added exceptions and cors

--- src/main/java/org/alantitor/config/CorsConfig.java
+++ src/main/java/org/alantitor/config/CorsConfig.java
@@ -1,2 +1,23 @@
-package org.alantitor.config;public class CorsConfig {
+package org.alantitor.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.frontUrl}")
+    private String frontUrl;
+
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        System.out.println(frontUrl);
+        corsRegistry.addMapping("/api/**")
+                .allowedOrigins(frontUrl)
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
--- src/main/java/org/alantitor/exception/NodeExceptionHandler.java
+++ src/main/java/org/alantitor/exception/NodeExceptionHandler.java
@@ -1,8 +1,18 @@
 package org.alantitor.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler
+public class NodeExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDataIntegrity(DataIntegrityViolationException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .build();
+    }
+
 }
